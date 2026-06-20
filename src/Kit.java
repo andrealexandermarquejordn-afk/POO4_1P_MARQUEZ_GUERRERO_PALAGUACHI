247115e34dd816372977439dd942373a01a9bfab
Andre_Marquez <andrealexander.marquejordn@gmail.com>
2026-06-20 14:58:52 -0500

    Implement Kit class with attributes and methods
    Added attributes and constructor for Kit class with getters and setters.

--- src/Kit.java
+++ src/Kit.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+
 public class Kit {
-    
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private ArrayList<String> codigosPartidosIncluidos;
+    private double precio;
+    private int disponibles;
+
+    public Kit(String codigo, String nombre, String descripcion, ArrayList<String> codigosPartidosIncluidos, double precio, int disponibles) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.codigosPartidosIncluidos = codigosPartidosIncluidos;
+        this.precio = precio;
+        this.disponibles = disponibles;
+    }
+
+    //Metodos getters y setters
+
+    public String getCodigo() {
+        return codigo; 
+    }
+    public String getNombre() { 
+        return nombre; 
+    }
+    public String getDescripcion() { 
+        return descripcion; }
+    public ArrayList<String> getCodigosPartidosIncluidos() { 
+        return codigosPartidosIncluidos; 
+    }
+    public double getPrecio() { 
+        return precio; 
+    }
+    public int getDisponibles() { 
+        return disponibles; 
+    }
+    public void setDisponibles(int disponibles) { 
+        this.disponibles = disponibles; 
+    }
 }
