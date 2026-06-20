2d38ee5a4e4956976b59e6f18d080f144fb5553c
CguerreroS07 <mauricioguerrero2007@gmail.com>
2026-06-20 16:30:05 -0500

    setrol

--- src/Usuario.java
+++ src/Usuario.java
@@ -87,6 +87,9 @@ public boolean iniciarSesion(String username, String password) {
         return rol;
     }
 
+    public void setRol(char rol) {
+        this.rol = rol;
+    }
 
     
         public String toString() {
