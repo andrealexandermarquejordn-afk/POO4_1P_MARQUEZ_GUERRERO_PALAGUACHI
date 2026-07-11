7a9f0e8b3a9c8ed59aebd6c8fd65b2e1f0cde8cb
Andre_Marquez <andrealexander.marquejordn@gmail.com>
2026-07-11 15:48:30 -0500

    Clase Aficionado

--- src/Aficionado.java
+++ src/Aficionado.java
@@ -1,3 +1,19 @@
 public class Aficionado extends Usuario{
-    
+    private String celular;
+    private String paisFavorito;
+    public Aficionado(String codigoUnico, String cedula, String nombres, String apellidos, String usuario, String contrasena, String correo, String rol, String celular, String paisFavorito){
+        super(codigoUnico, cedula, nombres, apellidos, usuario, contrasena, correo, rol);
+        this.celular = celular;
+        this.paisFavorito = paisFavorito;
+    }
+    @Override
+    public void consultarEntradas(){
+        System.out.println("Mis Compras (Aficionado: " + getNombres() + ")");
+    }
+    public String getCelular(){
+        return celular; 
+    }
+    public String getPaisFavorito(){
+        return paisFavorito; 
+    }
 }
