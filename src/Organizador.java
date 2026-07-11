ae3cf6b69b72256553f386712700472ee4c164bb
Andre_Marquez <andrealexander.marquejordn@gmail.com>
2026-07-11 15:51:08 -0500

    Clase Organizador

--- src/Organizador.java
+++ src/Organizador.java
@@ -1,3 +1,31 @@
-public class Organizador extends Usuario{
-    
+public class Organizador extends Usuario {
+    private String empresa;
+    private String cargo;
+
+    public Organizador(String codigoUnico, String cedula, String nombres, String apellidos, String usuario, String contrasena, String correo, String rol, String empresa, String cargo) {
+        super(codigoUnico, cedula, nombres, apellidos, usuario, contrasena, correo, rol);
+        this.empresa = empresa;
+        this.cargo = cargo;
+    }
+    @Override
+    public void consultarEntradas() {
+        System.out.println("===== CONSULTAR TODAS LAS COMPRAS (Modo Organizador) =====");
+        System.out.println("Organizador a cargo: " + getNombres() + " " + getApellidos() + " - " + this.cargo);
+    }
+    @Override
+    public String toString() {
+        return "Organizador: " + getNombres() + " " + getApellidos() + " | Empresa: " + empresa + " | Cargo: " + cargo;
+    }
+    public String getEmpresa() { 
+        return empresa; 
+    }
+    public void setEmpresa(String empresa) { 
+        this.empresa = empresa; 
+    }
+    public String getCargo() { 
+        return cargo; 
+    }
+    public void setCargo(String cargo) { 
+        this.cargo = cargo; 
+    }
 }
