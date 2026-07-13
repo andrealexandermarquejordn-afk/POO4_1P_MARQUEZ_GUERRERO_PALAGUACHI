5742c3338dd5ac2284cbb109b1f86e96b940c8d4
CguerreroS07 <mauricioguerrero2007@gmail.com>
2026-07-13 00:21:13 -0500

    Arreglo en clase java

--- src/Compra.java
+++ src/Compra.java
@@ -1,14 +1,16 @@
 import java.util.Date;
 public class Compra {
+
     private static int contadorCompras = 0; 
     private String codigoCompra;
-    private String tipo;
+    private TipoCompra tipo;
     private String codigoReferencia;
     private Date fechaCompra;
     private int cantidad;
     private double valorPagado;
     private String codigoAficionado;
-    public Compra(String tipo, String codigoReferencia, int cantidad, double valorPagado, String codigoAficionado){
+    
+    public Compra(TipoCompra tipo, String codigoReferencia, int cantidad, double valorPagado, String codigoAficionado){
         contadorCompras++;
         this.codigoCompra = "C" + String.format("%03d", contadorCompras); 
         this.tipo = tipo;
@@ -18,7 +20,7 @@ public class Compra {
         this.valorPagado = valorPagado;
         this.codigoAficionado = codigoAficionado;
     }
-    public Compra(String codigoCompra, String tipo, String codigoReferencia, Date fechaCompra, int cantidad, double valorPagado, String codigoAficionado){
+    public Compra(String codigoCompra, TipoCompra tipo, String codigoReferencia, Date fechaCompra, int cantidad, double valorPagado, String codigoAficionado){
         this.codigoCompra = codigoCompra;
         this.tipo = tipo;
         this.codigoReferencia = codigoReferencia;
@@ -31,10 +33,11 @@ public class Compra {
             contadorCompras = idNumerico;
         }
     }
+    //Metodos getters
     public String getCodigoCompra(){ 
         return codigoCompra;
     }
-    public String getTipo() { 
+    public TipoCompra getTipo() { 
         return tipo;
     }
     public String getCodigoReferencia() { 
@@ -51,5 +54,28 @@ public class Compra {
     }
     public String getCodigoAficionado() { 
         return codigoAficionado;
+    }
+
+    //Metodos setters
+    public void setCodigoCompra(String codigoCompra) {
+        this.codigoCompra = codigoCompra;
+    }
+    public void setTipo(TipoCompra tipo) {
+        this.tipo = tipo;
+    }
+    public void setCodigoReferencia(String codigoReferencia) {
+        this.codigoReferencia = codigoReferencia;
+    }
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public void setValorPagado(double valorPagado) {
+        this.valorPagado = valorPagado;
+    }
+    public void setCodigoAficionado(String codigoAficionado) {
+        this.codigoAficionado = codigoAficionado;
     }
 }
